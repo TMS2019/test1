058c57512113597ad21ee78e4802f6ac63ca5b32
perdana <perdanarobert@gmail.com>
2021-10-08 11:31:41 +0700

    first commit

---
+++ Controllers/M501_userController.cs
@@ -0,0 +1,45 @@
+﻿using CRUD_BAL.Service;
+using CRUD_DAL.Interface;
+using CRUD_DAL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDAspNetCore5WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class M501_userController : ControllerBase
+    {
+        private readonly PersonService _personService;
+
+        private readonly IRepository<Person> _Person;
+
+        public M501_userController(IRepository<Person> Person, PersonService ProductService)
+        {
+            _personService = ProductService;
+            _Person = Person;
+
+        }
+
+        [HttpGet("GetAllUser")]
+        public Object GetAllUsers()
+        {
+            var data = _personService.GetAllPersons();
+            var json = JsonConvert.SerializeObject(data, Formatting.Indented,
+                new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                }
+            );
+            return json;
+        }
+
+
+        
+    }
+}
